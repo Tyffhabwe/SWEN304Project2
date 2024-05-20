3cf0b912c9dee31fab04ace8bfda43934f7bfc46
Tyff <tyffhabwe@gmail.com>
2024-05-21 10:27:00 +1200

    Few basic commands are working

--- src/LibraryModel.java
+++ src/LibraryModel.java
@@ -5,25 +5,20 @@
  */
 
 import javax.swing.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class LibraryModel {
     //Credentials
     String url = "jdbc:postgresql://localhost:5433/LibraryDB";
-    String user = "postgres";
-    String password = "";
     Connection connection = null;
     // For use in creating dialogs and making them modal
     private JFrame dialogParent;
-
     public LibraryModel(JFrame parent, String userid, String password) {
 	    dialogParent = parent;
         try {
             Class.forName("org.postgresql.Driver");
 
-            connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, userid, password);
 
             if (connection != null) {
                 System.out.println("Successful connection to DB!");
@@ -36,43 +31,70 @@ public class LibraryModel {
         } catch (SQLException e) {
             System.out.println("DB Connection failed.");
             e.printStackTrace();
-        } finally {
-            if (connection!= null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
     public String bookLookup(int isbn) {
-	return "Lookup Book Stub";
+	    return "Lookup Book Stub";
     }
 
     public String showCatalogue() {
-	return "Show Catalogue Stub";
+	    return "Show Catalogue Stub";
     }
 
     public String showLoanedBooks() {
-	return "Show Loaned Books Stub";
+	    return "Show Loaned Books Stub";
     }
 
     public String showAuthor(int authorID) {
-	return "Show Author Stub";
+	    return "Show Author Stub";
     }
-
     public String showAllAuthors() {
-	return "Show All Authors Stub";
+        String sql = "SELECT " + Constants.AUTHOR_NAME + ", " + Constants.AUTHOR_SURNAME + " FROM author";
+        StringBuilder allAuthors = new StringBuilder("Show All Authors Stub");
+
+        try(Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            allAuthors.setLength(0);
+
+            while (resultSet.next()) {
+                String name = resultSet.getString(Constants.AUTHOR_NAME);
+                String surname = resultSet.getString(Constants.AUTHOR_SURNAME);
+                allAuthors.append(name).append("\t").append(surname).append('\n');
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to show all authors");
+            closeDBConnection();
+            e.printStackTrace();
+        }
+
+	    return allAuthors.toString();
     }
 
     public String showCustomer(int customerID) {
-	return "Show Customer Stub";
+	    return "Show Customer Stub";
     }
 
     public String showAllCustomers() {
-	return "Show All Customers Stub";
+        String sql = "SELECT " + Constants.CUSTOMER_LAST_NAME + ", " +
+                Constants.CUSTOMER_FIRST_NAME + " FROM customer";
+        StringBuilder allCustomers = new StringBuilder("Show All Customers Stub");
+
+        try(Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            allCustomers.setLength(0);
+
+            while(resultSet.next()) {
+                String lastName = resultSet.getString(Constants.CUSTOMER_LAST_NAME);
+                String firstName = resultSet.getString(Constants.CUSTOMER_FIRST_NAME);
+                allCustomers.append(firstName).append("\t").append(lastName).append('\n');
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to show all customers");
+            closeDBConnection();
+            e.printStackTrace();
+        }
+        return allCustomers.toString();
     }
 
     public String borrowBook(int isbn, int customerID,
@@ -84,7 +106,15 @@ public class LibraryModel {
 	return "Return Book Stub";
     }
 
-    public void closeDBConnection() {}
+    public void closeDBConnection() {
+        if (connection!= null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public String deleteCus(int customerID) {
     	return "Delete Customer";
@@ -97,4 +127,11 @@ public class LibraryModel {
     public String deleteBook(int isbn) {
     	return "Delete Book";
     }
+}
+
+class Constants {
+    public static final String AUTHOR_NAME = "name";
+    public static final String AUTHOR_SURNAME = "surname";
+    public static final String CUSTOMER_LAST_NAME = "l_name";
+    public static final String CUSTOMER_FIRST_NAME = "f_name";
 }
